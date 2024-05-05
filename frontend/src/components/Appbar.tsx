6a32908153cb86c311973cca719d8049136e80f7
MuthuKannan <muthukannan.muruga@gmail.com>
2024-05-06 00:38:51 +0530

    FE changes bug fix2

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,8 +1,9 @@
 import { useState, useRef, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export const Appbar = ({ UserInitial }: { UserInitial: string }) => {
   const router = useNavigate();
+  const location = useLocation();
   const dropdownRef = useRef<HTMLDivElement>(null); // Ref for the dropdown button
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -40,6 +41,8 @@ export const Appbar = ({ UserInitial }: { UserInitial: string }) => {
     };
   }, []);
 
+  const isWriteDisabled = location.pathname === '/postblog';
+
   return (
     <div className="px-10 my-5 border-b border-gray-150 pb-5 mb-10 flex justify-between items-center">
       <div className="font-semibold cursor-pointer">
@@ -48,8 +51,9 @@ export const Appbar = ({ UserInitial }: { UserInitial: string }) => {
       <div className="flex items-center space-x-4">
         <button
           type="button"
-          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          className={`text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 ${isWriteDisabled ? 'cursor-not-allowed' : ''}`}
           onClick={handleWriteClick}
+          disabled={isWriteDisabled}
         >
           Write
         </button>
